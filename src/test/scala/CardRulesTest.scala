0e5ab0380be640545d1253e300f7f371bdcc7f2b
Elvis020 <Spaceyel3@gmail.com>
2022-10-23 14:33:48 +0000

    Added additional testcases

---
+++ src/test/scala/CardRulesTest.scala
@@ -0,0 +1,35 @@
+import Actions.Actions.{dealCards, dealWithPlayers}
+import CardDeck.CardNumber.{Ace, Jack, King}
+import CardDeck.{Clubs, Spades, numberOfPlayers}
+import Utils.CardRules.{go_bust, hit, stick}
+import Utils.Player
+import munit.FunSuite
+
+import scala.collection.mutable.ListBuffer
+
+class CardRulesTest extends FunSuite{
+  // Attempt to Mock(might find a better approach to Mocking)
+  val player_1 = Player("player_1")
+  test("Total Cards player_1 has is 0") {
+    assertEquals(player_1.totalCardsOfPlayer.size, 0)
+  }
+
+
+  test("Total Cards player_1 has is 3, after the addition") {
+    player_1.totalCardsOfPlayer ++= ListBuffer(Clubs(King), Clubs(Jack), Spades(Ace))
+    assertEquals(player_1.totalCardsOfPlayer.size, 3)
+  }
+
+  test("Verifies that player_1 does not have a hit") {
+    val isHit = hit(player_1)
+    assertEquals(isHit, false)
+  }
+
+  test("Verifies that player_1 goes bust or is sticked") {
+    val isBusted = go_bust(player_1)
+    val isSticked = stick(player_1)
+    assertEquals(isBusted, true)
+    assertEquals(isSticked, true)
+  }
+
+}
---
+++ src/test/scala/GamePlayTest.scala
@@ -0,0 +1,32 @@
+import Actions.Actions.dealCards
+import CardDeck.numberOfPlayers
+import Utils.Player
+import munit.FunSuite
+
+import scala.collection.mutable.ListBuffer
+
+class GamePlayTest extends FunSuite{
+
+  // Mocking the players
+  val player_1 = Player("player_1")
+  val player_2 = Player("player_2")
+  val player_3 = Player("player_3")
+
+
+
+  test("Verifies player_1 has 2 cards after he is dealt with initially") {
+    dealCards(2, player_1)
+    assertEquals(player_1.totalCardsOfPlayer.size, 2)
+  }
+
+
+  test("Verifies player_1 has 3 cards after he is dealt again") {
+    dealCards(1, player_1)
+    assertEquals(player_1.totalCardsOfPlayer.size, 3)
+  }
+
+  test("Verifies that there are a total of 3 players") {
+    numberOfPlayers.addAll(ListBuffer(player_1,player_2,player_3))
+    assertEquals(numberOfPlayers.size, 3)
+  }
+}
