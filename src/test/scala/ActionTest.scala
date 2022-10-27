fabfa76d52f923c7a97fc0953682439f0b61f87a
Elvis020 <Spaceyel3@gmail.com>
2022-10-27 16:06:26 +0000

    Done with outstanding issues🥷

--- src/test/scala/ActionTest.scala
+++ src/test/scala/ActionTest.scala
@@ -1,4 +1,7 @@
-import Actions.Actions.{askUserTheShufflingTechnique, dealCardToPlayer, shuffleCards}
+import Actions.Actions._
+import CardDeck.CardNumber._
+import CardDeck._
+import Player.Player
 import Utils.TypeAlias.Card
 import munit.FunSuite
 
@@ -6,6 +9,25 @@ import java.io.{ByteArrayOutputStream, StringReader}
 import scala.collection.mutable.ListBuffer
 
 class ActionTest extends FunSuite {
+  override def afterAll(): Unit = numberOfPlayers.clear()
+
+  // Mocking the players
+  val player_1 = Player("Player1")
+  val player_2 = Player("Player2")
+  val player_3 = Player("Player3")
+
+  val card1 = ListBuffer(Hearts(CardNumber.Three), Diamond(CardNumber.Nine), Clubs(CardNumber.Ace))
+  val card2 = ListBuffer(Clubs(Four), Spades(Eight), Hearts(Two))
+  val card3 = ListBuffer(Diamond(Six), Clubs(Eight), Spades(Two))
+
+  // Adding cards to players
+  player_1.totalCardsOfPlayer.addAll(card1)
+  player_2.totalCardsOfPlayer.addAll(card2)
+  player_3.totalCardsOfPlayer.addAll(card3)
+  numberOfPlayers.addAll(ListBuffer(player_1, player_2, player_3))
+
+
+
   test("Total number of shuffled cards should be 52") {
     val numberOfCards = shuffleCards().size
     assertEquals(numberOfCards, 52)
@@ -24,7 +46,6 @@ class ActionTest extends FunSuite {
     val required = "Cards have been shuffled using the Pharoah/Faro Shuffle technique"
     assertEquals(actual,required)
   }
-
 
   test("Verifies that the Riffle shuffle technique is selected, when input is 1") {
     val inputParam = "1"
@@ -46,5 +67,25 @@ class ActionTest extends FunSuite {
     assertEquals(listOfCards.size, 1)
   }
 
-  // TODO: Mock the cards and see if logic of getting winner is correct
+  test("With the given data, the total number of probable players are 2") {
+    val actual = get_probable_winners(numberOfPlayers).size
+    assertEquals(actual,2)
+  }
+
+  test("With the given data, the probable players are Player2 and Player3") {
+    val actual = get_probable_winners(numberOfPlayers).map(_.name)
+    assertEquals(actual,ListBuffer("Player2","Player3"))
+  }
+
+  test("With the given data, the final winner is Player 3") {
+    val actual = getWinner(numberOfPlayers)
+    val out = new ByteArrayOutputStream()
+    Console.withOut(out){
+      getWinner(numberOfPlayers)
+    }
+    val winner = out.toString.split("\n").last.strip()
+    assertEquals(winner,"Player3")
+  }
+
+
 }
